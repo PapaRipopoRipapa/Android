1b5900a0a83d324e4ea231a6f2524173ce29b7f7
Bartosz Maslach <zal.kazachstan@gmail.com>
2016-10-13 23:37:10 +0200

    openGL activity

--- main/java/com/example/bartek/myapplication/MainActivity.java
+++ main/java/com/example/bartek/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.bartek.myapplication;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView mGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+
     }
 }
---
+++ main/java/com/example/bartek/myapplication/MyGLSurfaceView.java
@@ -0,0 +1,21 @@
+package com.example.bartek.myapplication;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by Bartek on 2016-10-13.
+ */
+public class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        setRenderer(mRenderer);
+    }
+}
