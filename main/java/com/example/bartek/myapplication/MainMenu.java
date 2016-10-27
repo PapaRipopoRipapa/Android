3f9a31d21aab935c03a2230cffcac562cc52d9c7
Bartosz Maslach <zal.kazachstan@gmail.com>
2016-10-27 23:53:15 +0200

    Add a welcome splashscreen

---
+++ main/java/com/example/bartek/myapplication/MainMenu.java
@@ -0,0 +1,10 @@
+package com.example.bartek.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by Bartek on 2016-10-27.
+ */
+
+public class MainMenu extends Activity{
+}
---
+++ main/java/com/example/bartek/myapplication/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.bartek.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bartek on 2016-10-27.
+ */
+
+public class WelcomeActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splashscreen);
+    }
+}
