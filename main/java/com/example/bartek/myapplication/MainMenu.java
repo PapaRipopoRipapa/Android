71ca4a92983bad7d293d843b5d1c6f66febd0aed
Bartosz Maslach <zal.kazachstan@gmail.com>
2016-10-28 19:36:36 +0200

    Added fading from splashscreen to main thread

--- main/java/com/example/bartek/myapplication/MainMenu.java
+++ main/java/com/example/bartek/myapplication/MainMenu.java
@@ -1,10 +1,17 @@
 package com.example.bartek.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Bartek on 2016-10-27.
  */
 
 public class MainMenu extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
 }
--- main/java/com/example/bartek/myapplication/WelcomeActivity.java
+++ main/java/com/example/bartek/myapplication/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.bartek.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by Bartek on 2016-10-27.
@@ -12,5 +14,16 @@ public class WelcomeActivity extends Activity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.splashscreen);
+
+        Thread mainThread = new Thread() {
+            public void run(){
+                Intent mainMenu = new Intent(WelcomeActivity.this, MainMenu.class);
+                WelcomeActivity.this.startActivity(mainMenu);
+                WelcomeActivity.this.finish();
+                overridePendingTransition(R.layout.fadein, R.layout.fadeout);
+            }
+        };
+
+        new Handler().postDelayed(mainThread, Engine.GAME_THREAD_DELAY);
     }
 }
